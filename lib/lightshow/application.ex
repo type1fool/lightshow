65b39d3e705b811a6a92ba1a1d3eb9fb4d8ddc3b
type1fool <historicstork@gmail.com>
2020-06-20 00:30:42 -0400

    mix format

--- lib/lightshow/application.ex
+++ lib/lightshow/application.ex
@@ -9,6 +9,7 @@ defmodule Lightshow.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Lightshow.Supervisor]
+
     children =
       [
         # Children for all targets
